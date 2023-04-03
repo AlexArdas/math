43199bc4a2647b96c92a253ce9b7b36559daad96
AlexArdas <asg270901@mail.ru>
2023-04-03 16:02:35 +0300

    math lab

---
+++ ReshenieSimpliks.py
@@ -0,0 +1,36 @@
+class Vector(object):
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __repr__(self):
+        return "({0}, {1})".format(self.x, self.y)
+
+    def __add__(self, other):
+        x = self.x + other.x
+        y = self.y + other.y
+        return Vector(x, y)
+
+    def __sub__(self, other):
+        x = self.x - other.x
+        y = self.y - other.y
+        return Vector(x, y)
+
+    def __rmul__(self, other):
+        x = self.x * other
+        y = self.y * other
+        return Vector(x, y)
+
+    def __truediv__(self, other):
+        x = self.x / other
+        y = self.y / other
+        return Vector(x, y)
+
+    def c(self):
+        return self.x, self.y
+
+
+def f(x):
+    x, y = x
+    z = y * x ** 0.5 - 2 * y ** 2 - x + 14 * y
+    return z
